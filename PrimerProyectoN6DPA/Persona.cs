399794888c5305371c854fc5bd51ad43bd6fcb14
Luis Chang <luis.chang@iugo.com.uy>
2022-09-04 13:34:22 -0500

    Agregar archivos de proyecto.

---
+++ PrimerProyectoN6DPA/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyectoN6DPA
+{
+    public class Persona
+    {
+        public int dni { get; set; }
+        public string nombreCompleto { get; set; }
+        public int edad { get; set; }
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(int dni, string nombreCompleto, int edad)
+        {
+            this.dni = dni;
+            this.nombreCompleto = nombreCompleto;
+            this.edad = edad;
+        }
+
+
+    }
+}
---
+++ PrimerProyectoN6DPA/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrimerProyectoN6DPA;
+
+Console.WriteLine("Hello, World!");
+
+string curso = "Desarrollo de ambiente web";
+int edad = 30;
+double salario = 1550.50;
+bool esActivo = false;
+
+var nuevoCurso = "Desarrollo de ambiente web 2022-02";
+var nuevaEdad = 30;
+var nuevoSalario = 1620.40;
+var esInactivo = true;
+
+var persona = new Persona();
+persona.dni = 44093892;
+persona.nombreCompleto = "Luis Chang";
+persona.edad = 30;
+
+
+
